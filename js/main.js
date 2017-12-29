620f55a356c3d17f3bcfd97e680a78d508c88755
lobkorea <lob@lob.kr>
2017-12-29 14:09:40 +0900

    수정사항 처리 1229

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function sorting(t_type){
 	if(t_type == 'all'){
-		$('.warning').show();
-		$('.dashboard').show();
-		$('.config').show();
-		$('.repair').show();
+		$('.class-warning').show();
+		$('.class-dashboard').show();
+		$('.class-config').show();
+		$('.class-repair').show();
 	}else{
-		$('.warning').hide();
-		$('.dashboard').hide();
-		$('.config').hide();
-		$('.repair').hide();
+		$('.class-warning').hide();
+		$('.class-dashboard').hide();
+		$('.class-config').hide();
+		$('.class-repair').hide();
 		$('.'+t_type).show();
 	}
 }
