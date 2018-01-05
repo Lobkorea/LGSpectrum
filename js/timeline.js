739eb3f3dd8e6ece1fb722f5350d1d3eb0367252
DESKTOP-PUAFIPK\USER <yoosb@lob.kr>
2018-01-05 17:05:24 +0900

    js & css commit

--- js/timeline.js
+++ js/timeline.js
@@ -1,14 +1,46 @@
-function sorting(t_type){
-	if(t_type == 'all'){
-		$('.class-warning').show();
-		$('.class-dashboard').show();
-		$('.class-config').show();
-		$('.class-repair').show();
-	}else{
-		$('.class-warning').hide();
-		$('.class-dashboard').hide();
-		$('.class-config').hide();
-		$('.class-repair').hide();
-		$('.'+t_type).show();
-	}
+function sorting(){
+	$('.class-warning').removeClass('class-off');
+	$('.class-warning').addClass('class-on');
+	$('.class-timelineHead01').removeClass('active');
+	$('.class-dashboard').removeClass('class-off');
+	$('.class-dashboard').addClass('class-on');
+	$('.class-timelineHead02').removeClass('active');
+	$('.class-repair').removeClass('class-off');
+	$('.class-repair').addClass('class-on');
+	$('.class-timelineHead03').removeClass('active');
+	$('.class-config').removeClass('class-off');
+	$('.class-config').addClass('class-on');
+	$('.class-timelineHead04').removeClass('active');
 }
+
+$(function() {
+    $('.class-timelineHead01').on('click', function() {
+        $(this).toggleClass('active ""');
+		$('.class-warning').toggleClass('class-off class-on');
+    });
+});
+
+$(function() {
+    $('.class-timelineHead02').on('click', function() {
+        $(this).toggleClass('active ""');
+		$('.class-dashboard').toggleClass('class-on class-off');
+    });
+});
+
+$(function() {
+    $('.class-timelineHead03').on('click', function() {
+        $(this).toggleClass('active ""');
+		$('.class-repair').toggleClass('class-on class-off');
+    });
+});
+
+$(function() {
+    $('.class-timelineHead04').on('click', function() {
+        $(this).toggleClass('active ""');
+		$('.class-config').toggleClass('class-on class-off');
+    });
+});
+
+$('document').ready(function(){
+	
+});
