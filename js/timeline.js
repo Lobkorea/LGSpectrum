06bfac1aab5611b09c94ff8c7c8b8b438b3db06f
DESKTOP-PUAFIPK\USER <yoosb@lob.kr>
2018-01-08 11:01:24 +0900

    font size and class rename

--- js/timeline.js
+++ js/timeline.js
@@ -1,42 +1,42 @@
 function sorting(){
 	$('.class-warning').removeClass('class-off');
 	$('.class-warning').addClass('class-on');
-	$('.class-timelineHead01').removeClass('active');
+	$('.class-timelineHead01').removeClass('class-active');
 	$('.class-dashboard').removeClass('class-off');
 	$('.class-dashboard').addClass('class-on');
-	$('.class-timelineHead02').removeClass('active');
+	$('.class-timelineHead02').removeClass('class-active');
 	$('.class-repair').removeClass('class-off');
 	$('.class-repair').addClass('class-on');
-	$('.class-timelineHead03').removeClass('active');
+	$('.class-timelineHead03').removeClass('class-active');
 	$('.class-config').removeClass('class-off');
 	$('.class-config').addClass('class-on');
-	$('.class-timelineHead04').removeClass('active');
+	$('.class-timelineHead04').removeClass('class-active');
 }
 
 $(function() {
     $('.class-timelineHead01').on('click', function() {
-        $(this).toggleClass('active ""');
+        $(this).toggleClass('class-active ""');
 		$('.class-warning').toggleClass('class-off class-on');
     });
 });
 
 $(function() {
     $('.class-timelineHead02').on('click', function() {
-        $(this).toggleClass('active ""');
+        $(this).toggleClass('class-active ""');
 		$('.class-dashboard').toggleClass('class-on class-off');
     });
 });
 
 $(function() {
     $('.class-timelineHead03').on('click', function() {
-        $(this).toggleClass('active ""');
+        $(this).toggleClass('class-active ""');
 		$('.class-repair').toggleClass('class-on class-off');
     });
 });
 
 $(function() {
     $('.class-timelineHead04').on('click', function() {
-        $(this).toggleClass('active ""');
+        $(this).toggleClass('class-active ""');
 		$('.class-config').toggleClass('class-on class-off');
     });
 });
